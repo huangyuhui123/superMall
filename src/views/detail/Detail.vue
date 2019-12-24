0a35d31179fafbcf3a1b0b5a7796c5d37b64dcff
黄玉慧 <2985380611@qq.com>
2019-12-24 17:43:10 +0800

    店铺信息

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -4,6 +4,7 @@
     </detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
     <detail-base-info :goods="goods"></detail-base-info>
+    <detail-shop-info :shop="shop"></detail-shop-info>
   </div>
 </template>
 
@@ -11,21 +12,24 @@
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
 import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
 
-import  {getDetail,Goods} from 'network/detail'
+import  {getDetail,Goods,Shop} from 'network/detail'
 	export default {
     name: "Detail",
     data(){
       return{
         id:null,
         topImages:[],
-        goods:{}
+        goods:{},
+        shop:{}
       }
     },
     components:{
       DetailNavBar,
       DetailSwiper,
-      DetailBaseInfo
+      DetailBaseInfo,
+      DetailShopInfo
     },
     created(){
       this.id = this.$route.params.id;
@@ -37,6 +41,8 @@ import  {getDetail,Goods} from 'network/detail'
         this.topImages = data.itemInfo.topImages;
         //2.获取商品信息
         this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+        //3.获取店铺的信心
+        this.shop = new Shop(data.shopInfo)
       })
 
       
---
+++ src/views/detail/childComps/DetailShopInfo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="shop-info">
+    <div class="shop-top">
+        <img :src="shop.logo" alt="">
+         <span class="title">{{shop.name}}</span>
+    </div>
+    <div class="shop-middle">
+        <div class="shop-middle-item shop-middle-left">
+            <div class="info-sells">
+                <div class="sells-count">
+                    {{shop-sells|sellCountFilter}}
+                </div>
+                <div class="sells-text">
+                    总销量
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+	export default {
+    name: "DetailShopInfo",
+    props:{
+        shop:{
+            type:Object,
+            default(){
+                return{}
+            }
+        }
+    }
+
+
+	}
+</script>
+
+<style scoped>
+ 
+</style>
