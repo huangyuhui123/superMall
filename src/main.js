4e7272f90354530cfb10145f804f59f4edaf701e
huangyuhui123 <2985380611@qq.com>
2019-12-28 10:06:25 +0800

    解决移动端300毫秒的延迟

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import router from './router/index';
 import store from './store/index';
 
 import toast from 'components/common/toast'
+import FastClick from 'fastclick';
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast);
-
+//解决移动端300毫秒的延迟
+FastClick.attach(document.body)
 new Vue({
   render: h => h(App),
   router,
