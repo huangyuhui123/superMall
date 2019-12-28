fcc3e45e880ba1cd8c09061131d64d817632136e
huangyuhui123 <2985380611@qq.com>
2019-12-28 08:23:27 +0800

    yyy

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -21,26 +20,4 @@ const store = new Vuex.Store({
 
 
 //3 挂在
-=======
-import Vue from 'vue';
-import Vuex from 'vuex';
-
-import mutations from './mutations';
-import actions from './actions';
-
-Vue.use(Vuex);
-
-const state ={
-    cartList:[]
-}
-
-const store = new Vuex.Store({
-    state,
-    mutations,
-    actions
-})
-
-
-//3 挂在
->>>>>>> c1f7ce4a398c5df2f8c85fec38de58fb071fdf80
 export default store
