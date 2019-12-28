d5bc08dd92f03af90d6b06419d6cd2190327c00a
huangyuhui123 <2985380611@qq.com>
2019-12-28 10:30:05 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -6,10 +6,19 @@ import store from './store/index';
 import toast from 'components/common/toast'
 import FastClick from 'fastclick';
 
+import VueLazyload from 'vue-lazyload'
+
+
+
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast);
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  loading: require('./assets/img/common/placeholder.png'),
+  attempt: 1
+})
 //解决移动端300毫秒的延迟
 FastClick.attach(document.body)
 new Vue({
